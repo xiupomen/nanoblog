2e4867c512321c034e4603461063505274f8e745
xiupomen <shakewave1@gmail.com>
2021-11-07 14:46:59 +0800

    clean nanoblog.py

--- nanoblog.py
+++ nanoblog.py
@@ -2,15 +2,12 @@ from app import create_app, db, cli
 from app.models import User, Post
 from pywebio.platform.flask import webio_view
 
-from tools import bmi, bkapp
+from tools import bmi
 
 app = create_app()
 cli.register(app)
 
 app.add_url_rule('/tools/bmi', 'bmi', webio_view(bmi),
-            methods=['GET', 'POST', 'OPTIONS'])
-
-app.add_url_rule('/tools/bokeh', 'bokeh', webio_view(bkapp),
             methods=['GET', 'POST', 'OPTIONS'])
 
 
